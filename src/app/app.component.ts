b96cf85e4126fa713fc26c50ef8d1f9361b39bcf
KietCV <kietcv@fpt.com>
2024-08-12 16:08:28 +0700

    firt build app flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   constructor() {}
   ngOnInit(): void {
-      console.log('app component initialized');
-  }
-  goToPictures(){
-
+    console.log('app component initialized');
   }
 }
